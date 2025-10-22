4b925f7f42de1cd29740a6b7ed323e7d48634f1d
Zhiqian-Zhangcc <1539990794@qq.com>
2025-10-22 11:02:24 +0800

    dwd
    dad

--- app-cities.py
+++ app-cities.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+plt.style.use('seaborn-v0_8')
 st.title('World Cites')
 df = pd.read_csv('worldcities.csv')
 
